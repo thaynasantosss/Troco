1deac6eebded98352952a02deadf920eb81a382b
thaynasantosss <thayna.2015.09@gmail.com>
2023-03-31 22:25:23 -0300

    Projeto Concluído

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿decimal valorCompra, valorPago, valorTroco;
+
+Console.WriteLine("*** Troco ***");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Valor total da compra (R$): ");
+valorCompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Valor recebido pelo caixa (R$): ");
+valorPago = Convert.ToDecimal(Console.ReadLine());
+
+valorTroco = valorPago - valorCompra;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"\nTroco: {valorTroco:C}");
+Console.ResetColor();
